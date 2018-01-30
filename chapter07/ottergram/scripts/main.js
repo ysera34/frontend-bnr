87926bfe588dcc530447d98312337be95c8fcd3e
ysera34 <ycyoon34@gmail.com>
2018-01-30 22:01:47 +0900

    chapter07 Visual Effects with CSS, transition

--- chapter07/ottergram/scripts/main.js
+++ chapter07/ottergram/scripts/main.js
@@ -1,7 +1,9 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tiny';
 var ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
@@ -52,7 +54,12 @@ function hideDetail() {
 
 function showDetail() {
   'use strict';
+  var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
   document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function() {
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  }, 50);
 }
 
 function addKeyPressHandler() {
