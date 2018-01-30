f88f3af72c62f65f68273fd6e444c9fcc46fbc1c
ysera34 <ycyoon34@gmail.com>
2018-01-30 21:20:22 +0900

    chapter07 Visual Effects with CSS, toggle class

--- chapter07/ottergram/scripts/main.js
+++ chapter07/ottergram/scripts/main.js
@@ -1,6 +1,8 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
   'use strict';
@@ -32,6 +34,7 @@ function addThumbClickHandler(thumb) {
   thumb.addEventListener('click', function(event) {
     event.preventDefault();
     setDetailsFromThumb(thumb);
+    showDetail();
   });
 }
 
@@ -42,10 +45,32 @@ function getThumbnailsArray() {
   return thumbnailArray;
 }
 
+function hideDetail() {
+  'use strict';
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetail() {
+  'use strict';
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+}
+
+function addKeyPressHandler() {
+  'use strict';
+  document.body.addEventListener('keyup', function(event) {
+    event.preventDefault();
+    console.log(event.keyCode);
+    if (event.keyCode === ESC_KEY) {
+      hideDetail();
+    }
+  });
+}
+
 function initializeEvents() {
   'use strict';
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
+  addKeyPressHandler();
 }
 
 initializeEvents();
