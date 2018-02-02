68051ef513c2e5890190b1998b28a3f6ad313cf6
ysera34 <ycyoon34@gmail.com>
2018-02-02 23:33:00 +0900

    chapter08 modules, objects and methods, constructor prototype

--- chapter08/coffeerun/scripts/datastore.js
+++ chapter08/coffeerun/scripts/datastore.js
@@ -5,7 +5,24 @@
 
   function DataStore() {
     console.log('running the DataStore function');
+    this.data = {};
   }
+
+  DataStore.prototype.add = function(key, val) {
+    this.data[key] = val;
+  };
+
+  DataStore.prototype.get = function(key) {
+    return this.data[key];
+  };
+
+  DataStore.prototype.getAll = function() {
+    return this.data;
+  };
+
+  DataStore.prototype.remove = function(key) {
+    delete this.data[key];
+  };
 
   App.DataStore = DataStore;
   window.App = App;
