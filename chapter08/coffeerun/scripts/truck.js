e36dce53f3c24d60a9396187352087766c85992c
ysera34 <ycyoon34@gmail.com>
2018-02-03 09:50:18 +0900

    chapter08 modules, objects and methods, debugging

--- chapter08/coffeerun/scripts/truck.js
+++ chapter08/coffeerun/scripts/truck.js
@@ -17,6 +17,15 @@
     this.db.remove(customerId);
   };
 
+  Truck.prototype.printOrders = function() {
+    var customerIdArray = Object.keys(this.db.getAll());
+
+    console.log('Truck #' + this.truckId + ' has pending orders:');
+    customerIdArray.forEach(function(id) {
+      console.log(this.db.get(id));
+    }.bind(this));
+  };
+
   App.Truck = Truck;
   window.App = App;
 })(window);
