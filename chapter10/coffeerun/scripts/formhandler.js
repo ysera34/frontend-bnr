1de6ab1cc3c0cd97e54de36446235e9927670b77
ysera34 <ycyoon34@gmail.com>
2018-02-10 15:00:38 +0900

    chapter10 processing forms with javascript, use formhandler

--- chapter10/coffeerun/scripts/formhandler.js
+++ chapter10/coffeerun/scripts/formhandler.js
@@ -15,6 +15,24 @@
     }
   }
 
+  FormHandler.prototype.addSubmitHandler = function(fn) {
+    console.log('Setting submit handler for form');
+
+    this.$formElement.on('submit', function(event) {
+      event.preventDefault();
+
+      // var data = $(this).serializeArray();
+      // console.log(data);
+      var data = {};
+      $(this).serializeArray().forEach(function(item) {
+        data[item.name] = item.value;
+        console.log(item.name + ' is ' + item.value);
+      });
+      console.log(data);
+      fn(data);
+    });
+  };
+
   App.FormHandler = FormHandler;
   window.App = App;
 
