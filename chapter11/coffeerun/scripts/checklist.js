de74b0b6cc9ece80a5f674d9abe4b1947c5f9900
ysera34 <ycyoon34@gmail.com>
2018-02-14 23:31:26 +0900

    chapter11 from data to DOM, checklist remove row

--- chapter11/coffeerun/scripts/checklist.js
+++ chapter11/coffeerun/scripts/checklist.js
@@ -15,12 +15,28 @@
     }
   }
 
+  CheckList.prototype.addClickHandler = function(fn) {
+    this.$element.on('click', 'input', function(event) {
+      var email = event.target.value;
+      this.removeRow(email);
+      fn(email);
+    }.bind(this));
+  };
+
   CheckList.prototype.addRow = function(coffeeOrder) {
+    // Remove any existing rows that match the email address
+    this.removeRow(coffeeOrder.emailAddress);
+
     // Create a new instance of a row, using the coffee order info
     var rowElement = new Row(coffeeOrder);
 
     // Add the new row instance's $element property to the checklist
     this.$element.append(rowElement.$element);
+  };
+
+  CheckList.prototype.removeRow = function(email) {
+    this.$element.find('[value="' + email + '"]')
+    .closest('[data-coffee-order="checkbox"]').remove();
   };
 
   function Row(coffeeOrder) {
