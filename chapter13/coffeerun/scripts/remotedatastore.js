fe65d4c149f755fa3c11f21e875bf90bf49d1017
ysera34 <ycyoon34@gmail.com>
2018-03-04 22:20:04 +0900

    chapter13 ajax post request to server

---
+++ chapter13/coffeerun/scripts/remotedatastore.js
@@ -0,0 +1,24 @@
+(function(window) {
+  'use strict';
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  function RemoteDataStore(url) {
+    if (!url) {
+      throw new Error('No remote URL supplied');
+    }
+
+    this.serverUrl = url;
+  }
+
+  RemoteDataStore.prototype.add = function (key, val) {
+    // Code will go here
+    $.post(this.serverUrl, val, function (serverResponse) {
+      console.log(serverResponse);
+    });
+  };
+
+  App.RemoteDataStore = RemoteDataStore;
+  window.App = App;
+
+})(window);
