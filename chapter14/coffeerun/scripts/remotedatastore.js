81a18b044b411e97ee348030ae728b17937e543a
ysera34 <ycyoon34@gmail.com>
2018-03-09 00:18:08 +0900

    chapter14 deferreds and promises, return deferred

--- chapter14/coffeerun/scripts/remotedatastore.js
+++ chapter14/coffeerun/scripts/remotedatastore.js
@@ -12,29 +12,31 @@
   }
 
   RemoteDataStore.prototype.add = function (key, val) {
-    // Code will go here
-    $.post(this.serverUrl, val, function (serverResponse) {
+    return $.post(this.serverUrl, val, function (serverResponse) {
       console.log(serverResponse);
     });
   };
 
   RemoteDataStore.prototype.getAll = function (cb) {
-    // Code will go here
-    $.get(this.serverUrl, function (serverResponse) {
-      console.log(serverResponse);
-      cb(serverResponse);
+    return $.get(this.serverUrl, function (serverResponse) {
+      if (cb) {
+        console.log(serverResponse);
+        cb(serverResponse);
+      }
     });
   };
 
   RemoteDataStore.prototype.get = function (key, cb) {
-    $.get(this.serverUrl + '/' + key, function(serverResponse) {
-      console.log(serverResponse);
-      cb(serverResponse);
+    return $.get(this.serverUrl + '/' + key, function(serverResponse) {
+      if (cb) {
+        console.log(serverResponse);
+        cb(serverResponse);
+      }
     });
   };
 
   RemoteDataStore.prototype.remove = function(key) {
-    $.ajax(this.serverUrl + '/' + key, {
+    return $.ajax(this.serverUrl + '/' + key, {
       type: 'DELETE'
     });
   };
--- chapter14/coffeerun/scripts/truck.js
+++ chapter14/coffeerun/scripts/truck.js
@@ -9,12 +9,12 @@
 
   Truck.prototype.createOrder = function(order) {
     console.log('Adding order for ' + order.emailAddress);
-    this.db.add(order.emailAddress, order);
+    return this.db.add(order.emailAddress, order);
   };
 
   Truck.prototype.deliverOrder = function(customerId) {
     console.log('Delivering order for ' + customerId);
-    this.db.remove(customerId);
+    return this.db.remove(customerId);
   };
 
   Truck.prototype.printOrders = function() {
