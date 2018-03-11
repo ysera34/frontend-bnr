15d24c7e61d96de45ae4a2f5e9d19d85966e8eeb
ysera34 <ycyoon34@gmail.com>
2018-03-11 21:58:44 +0900

    chapter15 introduction to node.js, file path

--- chapter15/chattrbox/index.js
+++ chapter15/chattrbox/index.js
@@ -1,7 +1,23 @@
 var http = require('http');
+var fs = require('fs');
+// var path = require('path');
+var extract = require('./path');
 
 var server = http.createServer(function(req, res) {
   console.log('Responding to a request.');
-  res.end('<h1>Hello, World</h1>');
+  // var url = req.url;
+  //
+  // var fileName = 'index.html';
+  // if (url.length > 1) {
+  //   fileName = url.substring(1);
+  // }
+  // console.log(fileName);
+  // var filePath = path.resolve(__dirname, 'app', fileName);
+  var filePath = extract(req.url);
+
+  // res.end('<h1>Hello, World</h1>');
+  fs.readFile(/* 'app/index.html' */ filePath, function(err, data) {
+    res.end(data);
+  });
 });
 server.listen(3000);
