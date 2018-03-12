8b6b6bc53e596c2f381084adf0848af2f1b419ab
ysera34 <ycyoon34@gmail.com>
2018-03-13 00:44:41 +0900

    chapter16 real-time communication with websocket, websocket server chatting

--- chapter16/chattrbox/websockets-server.js
+++ chapter16/chattrbox/websockets-server.js
@@ -4,14 +4,24 @@ var port = 3001;
 var ws = new WebSocketServer({
   port: port
 });
+var messages = [];
 
 console.log('websockets server started');
 
 ws.on('connection', function(socket) {
   console.log('client connection established');
 
+  messages.forEach(function(message) {
+    socket.send(message);
+  });
+
   socket.on('message', function(data) {
     console.log('message received: ' + data);
-    socket.send(data);
+    messages.push(data);
+    ws.clients.forEach(function(clientSocket) {
+      clientSocket.send(data);
+    });
+
+    // socket.send(data);
   });
 });
